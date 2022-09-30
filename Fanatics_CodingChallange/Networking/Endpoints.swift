3e3474d7a7e15f67296bfb92476a528219be2249
Krunal Mistry <krunalmistry119@gmail.com>
2022-09-29 23:45:07 -0500

    removing private token

--- Fanatics_CodingChallange/Networking/Endpoints.swift
+++ Fanatics_CodingChallange/Networking/Endpoints.swift
@@ -13,5 +13,5 @@ enum Endpoints: String {
 
 enum AuthToken {
     // Need to add tocken after signup on given API webpage, I have removed the tocken because it was linked to my account
-    static let tocken = "886e220766def96f73568c7ccf2fa19729bd6458db591ba5c52dea373664de1e"
+    static let tocken = ""
 }
